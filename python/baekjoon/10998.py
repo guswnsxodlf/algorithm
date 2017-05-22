bbc3340515d24c7bab3e1df99c186a98b74158e2
전현준 <guswnsxodlf@gmail.com>
2017-05-22 22:04:43 +0900

    10998 11718

---
+++ python/baekjoon/10998.py
@@ -0,0 +1,4 @@
+# Created by Jello on 2017. 5. 22.
+
+a = list(map(int, str(input()).split(' ')))
+print(a[0]*a[1])
---
+++ python/baekjoon/11718.py
@@ -0,0 +1,7 @@
+# Created by Jello on 2017. 5. 22.
+
+while True:
+    try:
+        print(str(input()))
+    except EOFError:
+        break
