98635e61bf0636fd35f1cd8a5d38d2b668e25da6
전현준 <guswnsxodlf@gmail.com>
2017-06-01 15:48:58 +0900

    2292

--- python/baekjoon/2292.py
+++ python/baekjoon/2292.py
@@ -1,4 +1,14 @@
 # Created by Jello on 2017. 5. 22.
 import math
 a = int(input())
-print(math.ceil((a-1)/6)+1)
+
+cal = math.ceil((a-1)/6)
+i = 1
+sum_i = 0
+while True:
+    if cal <= sum_i:
+        break
+    sum_i += i
+    i += 1
+
+print(i)
