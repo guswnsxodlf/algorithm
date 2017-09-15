b475fc5d180260b8c34ca2bab23d6bde6d10bd03
전현준 <guswnsxodlf@gmail.com>
2017-09-15 17:04:40 +0900

    14616

--- python/baekjoon/14616.py
+++ python/baekjoon/14616.py
@@ -1,5 +1,17 @@
 # Created by Jello on 2017. 9. 14.
 import sys
+import bisect
+
+def is_destroyed(r1, r2):
+	r1_bs = bisect.bisect_left(laser, r1)
+	r2_bs = bisect.bisect_left(laser, r2)
+
+	if r1_bs != r2_bs:
+		return True
+	elif (r1_bs < len(laser) and r2_bs < len(laser)) and (laser[r1_bs] == r1 or laser[r2_bs] == r2):
+		return True
+	else:
+		return False
 
 def getTangent(x, y):
     return y/x
@@ -7,11 +19,12 @@ def getTangent(x, y):
 N = int(sys.stdin.readline())
 radioactive = []
 laser = []
+cnt = 0
 
 for i in range(N):
     data = list(map(int, sys.stdin.readline().strip().split(' ')))
-    data_arr = [getTangent(data[0], data[1]), getTangent(data[2], data[3])]
-    radioactive.append([min(data_arr), max(data_arr)])
+    radioactive.append([getTangent(data[0], data[1]), getTangent(data[2], data[3])])
+    print([getTangent(data[0], data[1]), getTangent(data[2], data[3])])
 
 M = int(sys.stdin.readline())
 
@@ -19,3 +32,12 @@ for i in range(M):
     data = list(map(int, sys.stdin.readline().strip().split(' ')))
     laser.append(getTangent(data[0], data[1]))
 
+laser.sort()
+
+for i in range(N):
+	print(radioactive[i][0], radioactive[i][1])
+	if is_destroyed(radioactive[i][0], radioactive[i][1]):
+		print('is_destroyed')
+		cnt += 1
+
+print(len(radioactive)-cnt)
